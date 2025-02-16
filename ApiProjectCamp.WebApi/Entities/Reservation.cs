5aa2492be781aed59beaf8ca9787591ea2f739b9
velibeyli <valibayli91@gmail.com>
2025-02-16 20:24:35 +0400

    Entities added

---
+++ ApiProjectCamp.WebApi/Entities/Reservation.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiProjectCamp.WebApi.Entities
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string ReservationTime { get; set; }
+        public int CountOfPeople { get; set; }
+        public string Message { get; set; }
+        public string ReservationStatus { get; set; }
+
+    }
+}
